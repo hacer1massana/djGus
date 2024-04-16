643308a42cd23c72a233dac772bbc7fe663ef659
39l6 <130191016+39l6@users.noreply.github.com>
2024-04-15 21:20:31 -0600

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const audioContext = new AudioContext();
-let osc = audioContext.createOscillator();
-osc.type = "sine";
-osc.connect(audioContext.destination);
-osc.start(audioContext.currentTime);
